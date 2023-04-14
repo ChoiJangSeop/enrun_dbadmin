09fc82f7e9c9eda4dd390b26205b92a8fa1331b1
jangseop <kgh010529@naver.com>
2023-04-14 11:19:33 +0900

    complete deploy version :)

--- src/main/java/gam/jangseop/dbadmin/DbAdminApplication.java
+++ src/main/java/gam/jangseop/dbadmin/DbAdminApplication.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
@@ -12,11 +14,15 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
 @SpringBootApplication
-
-public class DbAdminApplication {
+public class DbAdminApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DbAdminApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DbAdminApplication.class);
 	}
 
 }
--- src/main/java/gam/jangseop/dbadmin/controller/TestController.java
+++ src/main/java/gam/jangseop/dbadmin/controller/TestController.java
@@ -1,2 +1,19 @@
-package gam.jangseop.dbadmin.controller;public class TestController {
+package gam.jangseop.dbadmin.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@CrossOrigin(origins = "http://localhost:5173")
+public class TestController {
+
+    @GetMapping("/")
+    public String test() {
+        return "Hello World!";
+    }
 }
